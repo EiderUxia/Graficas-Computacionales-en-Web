dc4c9c740f0401e349640c66782b706596c65a66
Fabiola <fabiola-2803@hotmail.com>
2021-09-01 01:19:24 -0500

    CANVA
    Se agrego canva y la tabla con los nombres

--- js/JavaScript.js
+++ js/JavaScript.js
@@ -2,14 +2,28 @@ $(document).ready(function () {
 
         var Bienvenido = document.getElementById("Bienvenido");
         var Cargando = document.getElementById("Cargando");
+        var Contenedor = document.getElementById("contenedor");
+        var Canvas = document.getElementById("contCanvas");
         Bienvenido.style.visibility = "visible";
 
         $("#btnEnviar").click(function(){
                 var nombre = document.getElementById("txtName").value;
                 if(nombre!="") {
                         $("#Saludo").text("Hola " + nombre);
+                        setTimeout(function(){
                                 Bienvenido.style.visibility = "hidden";
+                                Bienvenido.style.opacity = 0;
                                 Cargando.style.visibility = "visible";
+                                Cargando.style.opacity = 1;
+                        }, 500);
+                        setTimeout(function(){
+                                Contenedor.style.visibility = "hidden";
+                                Contenedor.style.opacity = 0;
+                                Canvas.style.visibility = "visible";
+                                Canvas.style.opacity = 1;
+                        }, 2000);
+
+                                
                 }
                 else{
                         alert("Por favor escriba su nombre");

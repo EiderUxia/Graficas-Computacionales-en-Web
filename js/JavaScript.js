d2d80ec8d5934ebe0429f499f1092411ade3cc41
Fabiola <fabiola-2803@hotmail.com>
2021-10-19 14:33:25 -0500

    corrección

--- js/JavaScript.js
+++ js/JavaScript.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-
         var Bienvenido = document.getElementById("Bienvenido");
         var Cargando = document.getElementById("Cargando");
         var Contenedor = document.getElementById("contenedor");
@@ -11,80 +10,68 @@ $(document).ready(function () {
 
         Bienvenido.style.visibility = "visible";
         var NombreSave;
-        $("#btnEnviar").click(function(){
-                var nombre = document.getElementById("txtName").value;
-                if(nombre!="") {
-                        /*
-                        $("#Saludo").text("Hola " + nombre);
-                        setTimeout(function(){
+        var NombreSave2;
+        $("#btnEnviar").click(function () {
+                var nombre = document.getElementById("input").value;
+                var nombre2 = document.getElementById("input2").value;
+                if (nombre != "") {
+                        if (nombre2 != "") {
                                 Bienvenido.style.visibility = "hidden";
                                 Bienvenido.style.opacity = 0;
-                                Cargando.style.visibility = "visible";
-                                Cargando.style.opacity = 1;
-                        }, 500);
-                        setTimeout(function(){
-                                Contenedor.style.visibility = "hidden";
-                                Contenedor.style.opacity = 0;
-                                Canvas.style.visibility = "visible";
-                                Canvas.style.opacity = 1;
-                        }, 2000);
-                        */
-                        Bienvenido.style.visibility = "hidden";
-                        Bienvenido.style.opacity = 0;
-                        Dificultad.style.visibility = "visible";
-                        Dificultad.style.opacity = 1;
-                        NombreSave = nombre;
-                }
-                else{
-                        alert("Por favor escriba su nombre");
+                                Dificultad.style.visibility = "visible";
+                                Dificultad.style.opacity = 1;
+                                NombreSave = nombre;
+                                NombreSave2 = nombre2;
+                        } else {
+                                alert("Falta nombre del jugador 2");
+                        }
+                } else {
+                        alert("Falta nombre del jugador 1");
                 }
         });
 
-
-        $("#SelecNormal").click(function(){                
-                setTimeout(function(){
-                        $("#Saludo").text("Hola " + NombreSave);
+        $("#SelecNormal").click(function () {
+                setTimeout(function () {
+                        $("#Saludo").text("Hola " + NombreSave + " y " + NombreSave2);
                         Dificultad.style.visibility = "hidden";
                         Dificultad.style.opacity = 0;
                         Cargando.style.visibility = "visible";
                         Cargando.style.opacity = 1;
                 }, 500);
-                setTimeout(function(){
+                setTimeout(function () {
                         Contenedor.style.visibility = "hidden";
                         Contenedor.style.opacity = 0;
                         Canvas.style.visibility = "visible";
                         Canvas.style.opacity = 1;
                 }, 2000);
         });
-        $("#SelecDificil").click(function(){                
-                setTimeout(function(){
-                        $("#Saludo").text("Hola " + NombreSave);
+        $("#SelecDificil").click(function () {
+                setTimeout(function () {
+                        $("#Saludo").text("Hola " + NombreSave + " y " + NombreSave2);
                         Dificultad.style.visibility = "hidden";
                         Dificultad.style.opacity = 0;
                         Cargando.style.visibility = "visible";
                         Cargando.style.opacity = 1;
                 }, 500);
-                setTimeout(function(){
+                setTimeout(function () {
                         Contenedor.style.visibility = "hidden";
                         Contenedor.style.opacity = 0;
                         Iconos.style.visibility = "visible";
                         Iconos.style.opacity = 1;
                         Canvas.style.visibility = "visible";
                         Canvas.style.opacity = 1;
-
                 }, 2000);
         });
 
-
-        $("#config ").click(function(){                
-                setTimeout(function(){
+        $("#config ").click(function () {
+                setTimeout(function () {
                         modal1.style.visibility = "visible";
                         modal1.style.opacity = 1;
                 }, 500);
         });
 
-        $("#configuracion").click(function(){                
-                setTimeout(function(){
+        $("#configuracion").click(function () {
+                setTimeout(function () {
                         modal1.style.visibility = "hidden";
                         modal1.style.opacity = 0;
                 }, 500);
